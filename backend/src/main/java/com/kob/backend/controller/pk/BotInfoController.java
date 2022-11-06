bcdfb644ae43b83349b080df426a9e0579ace5fd
wakakaxz <wakakaxiaozheng@163.com>
2022-11-06 21:41:12 +0800

    前端环境搭建和测试

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,10 +3,19 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/pk/")
+@RequestMapping("pk")
 public class BotInfoController {
 
-
+    @RequestMapping("getBotInfo")
+    public Map<String, String> getBotInfo() {
+        Map<String , String> map = new HashMap<>();
+        map.put("name", "tiger");
+        map.put("rating", "1500");
+        return map;
+    }
 
 }
