09699c2d6e622ead4ac40e8e5f9bb87fe8e07bd3
wakakaxz <wakakaxiaozheng@163.com>
2022-11-06 19:30:25 +0800

    创建后端

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,12 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+
+
+}
